9f9910996ff4cdc4467bc64448e5ffc169908167
Brenda Pereira Camara <brendacamara@outlook.com>
2023-08-30 14:20:25 -0300

    refactor to work with node 10

--- karatsuba-final.js
+++ karatsuba-final.js
@@ -1,7 +1,6 @@
-
 class KaratsubaCalculator {
 
-  // divide a string em 3 partes   
+  // divide a string em 3 partes
   divideIntoThreeParts(arg) {
     const partsize = Math.ceil(arg.length / 3);
     const parts = [];
@@ -15,11 +14,11 @@ class KaratsubaCalculator {
 
   // faz a soma intermediária dos numeros de acordo com o algoritmo de karatsuba
   sum(a, b) {
-    let carry = 0;
+    let carry =0;
     let result = "";
 
     for (let i = 0; i < Math.max(a.length, b.length) || carry; i++) {
-      const digitA = Number(a[a.length - 1 - i]) || 0;
+      const digitA = Number(a[a.length - 1 - i]) || 0;     
       const digitB = Number(b[b.length - 1 - i]) || 0;
       const sum = digitA + digitB + carry;
 
@@ -31,7 +30,7 @@ class KaratsubaCalculator {
   }
 
   multiply(s, p) {
-    // se chegou no final onde cada string só tem 1 digito faz a multiplicação simples e retorna
+    // se chegou no final onde cada string só tem 1 digito faz a multiplicação simples retorna
     if (s.length === 1 && p.length === 1) {
       return String(Number(s) * Number(p));
     }
@@ -42,12 +41,12 @@ class KaratsubaCalculator {
     // calcula a diferença de tamanho dos numeros e adicina zeros no final
     const e = [
       s.length - a[0].length,
-      s.length - (a[0].length + (a[1]?.length || 0)),
+      s.length - (a[0].length + (a[1] ? a[1].length : 0)),
       0,
     ];
     const f = [
       p.length - b[0].length,
-      p.length - (b[0].length + (b[1]?.length || 0)),
+      p.length - (b[0].length + (b[1] ? b[1].length : 0)),
       0,
     ];
 
@@ -67,23 +66,18 @@ class KaratsubaCalculator {
   }
 
   // função principal de entrada
-  calculate(a, b) {
+ calculate(a, b) {
     return this.multiply(a, b);
   }
 }
 
 // TESTE:
-const process = require("process");
-var entrada = process.argv;
-
 const calculator = new KaratsubaCalculator();
 const result = calculator.calculate(
-  entrada[2],
-  entrada[3]
+  process.argv[2],
+  process.argv[3]
 );
 console.log(result);
 
-// rodar com:  node .\karatsuba-final.js 12432134341245674745675476 7054920058988836008343024 
-// obs: tem que ter o node instalado :) 
-
-// Brenda Pereira Camara'
+// Rodar com:  node karatsuba-final.js 12432134341245674745675476 7054920058988836008343024 
+// obs: tem que ter o node instalado :)
